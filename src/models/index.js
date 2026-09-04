a242e40e52df386fc331d502ce31466b1122e4c1
Matheus Augusto Rodrigues de Alencar <matheus06alencar@gmail.com>
2026-09-04 13:59:19 -0300

    feat: configura relacionamento entre matérias e tarefas

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,16 @@
 const Materia = require('./Materia');
+const Tarefa = require('./Tarefa');
+
+Materia.hasMany(Tarefa, {
+    foreignKey: 'materiaId',
+    onDelete: 'CASCADE'
+});
+
+Tarefa.belongsTo(Materia, {
+    foreignKey: 'materiaId'
+});
 
 module.exports = {
-    Materia
+    Materia,
+    Tarefa
 };

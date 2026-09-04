05247867776c9a880491a91cec9c5d5b58981f2b
Matheus Augusto Rodrigues de Alencar <matheus06alencar@gmail.com>
2026-09-04 13:59:22 -0300

    feat: registra rotas de tarefas

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { engine } = require('express-handlebars');
 const sequelize = require('../database/database');
 const materiasRoutes = require('./routes/materias');
+const tarefasRoutes = require('./routes/tarefas');
 
 require('./models');
 const app = express();
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(express.static('src/public'));
 
 app.use('/materias', materiasRoutes);
+app.use('/tarefas', tarefasRoutes);
 
 app.get('/', (req, res) => {
     res.render('home', { layout: false });
